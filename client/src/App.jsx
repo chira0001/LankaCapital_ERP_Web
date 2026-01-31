b77c592e0c80c86a43e9ce39de2b29fa9cedb0cd
Chirath <chirathwijesinghe123@gmail.com>
2026-01-31 21:11:24 +0530

    navbar implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import AdminNavbar from './component/Navbar/AdminNavbar'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className=''>
+      <AdminNavbar />
+    </div>
   )
 }
 
---
+++ client/src/component/Navbar/AdminNavbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, Links, useNavigate } from "react-router-dom";
+
+
+const AdminNavbar = () => {
+    return (
+        <div className="bg-white w-screen text-black shadow-lg mb-8">
+            <nav>
+                <div>Home</div>
+                <div>Home</div>
+                <div>Home</div>
+            </nav>
+        </div>
+    )
+}
+
+export default AdminNavbar
