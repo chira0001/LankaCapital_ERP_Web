0a7b33b594c92dab55620319e3c4858a313ac4b4
Chirath <chirathwijesinghe123@gmail.com>
2026-04-19 23:37:00 +0530

    Backend initialized

--- server/src/main/java/com/lankacapital/server/ServerApplication.java
+++ server/src/main/java/com/lankacapital/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.lankacapital.server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL",dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+		System.setProperty("SECRET_STRING",dotenv.get("SECRET_STRING"));
+
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
