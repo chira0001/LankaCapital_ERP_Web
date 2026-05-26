ada1217b3f1279adb935628fe2ef250613ade7e5
Vihaga256 <vihagayasithawijewardhana29@gmail.com>
2026-05-26 16:53:00 +0530

    create end point and Async methods

---
+++ server/src/main/java/com/lankacapital/server/dtos/CustomerResAsyncDto.java
@@ -0,0 +1,12 @@
+package com.lankacapital.server.dtos;
+
+import lombok.Data;
+
+@Data
+public class CustomerResAsyncDto {
+    private long nic;
+    private String name;
+    private String email;
+    private String address;
+    private String phone_number;
+}
---
+++ server/src/main/java/com/lankacapital/server/dtos/FieldOfficerAsyncDto.java
@@ -0,0 +1,10 @@
+package com.lankacapital.server.dtos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class FieldOfficerAsyncDto {
+    private List<Long> id;
+}
